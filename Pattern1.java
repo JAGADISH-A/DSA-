09d33cf41a93a4841914d2271dd843770038f90b
JAGADISH-A <jagadishchandirabosea@gmail.com>
2026-06-06 13:49:11 +0530

    chore : correct version of sliding pattern

--- Pattern1.java
+++ Pattern1.java
@@ -14,7 +14,7 @@ class Slidingpattern
     {
         for(int i = 0 ; i < n ; i++)
         {
-            for(int j = 0 ; j < n ; j++)
+            for(int j = 0 ; j<=i; j++)
             {
                 System.out.print(" * ");
             }
