3fbce0d4f8b02a5f88a3caedb99313c333adae91
JAGADISH-A <jagadishchandirabosea@gmail.com>
2026-06-08 01:47:05 +0530

    feat : diamond pattern is completed successfully

---
+++ diamond.java
@@ -0,0 +1,54 @@
+import java.util.*;
+class pattern 
+{
+    void pattern(int n)
+    {
+        int i;
+        int j ;
+   
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n-i-1;j++)
+            {
+                System.out.print(" ");
+            }
+            for(j=0;j<(2*i+1);j++)
+            {
+                System.out.print("*");
+            }
+            for(j=0;j<n-i-1;j++)
+            {
+                System.out.print(" ");
+            }
+            
+            System.out.println();
+
+        }
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(j=0;j<((n*2)-(i*2+1));j++)
+            {
+                System.out.print("*");
+            }
+            for(j=0;j<i;j++)
+            {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
+public class diamond
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pattern p = new pattern();
+        p.pattern(n);
+    }
+}
